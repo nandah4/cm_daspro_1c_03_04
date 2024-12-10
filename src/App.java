1bea4cdc357fac14eb634bfbadfd4e94f0b728f3
nandah4 <nyustira@gmail.com>
2024-12-10 11:15:31 +0700

    feat: structur loop main menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        OUTER_LOOP: while (true) {
+            System.out.println("===== MENU INVENTORI KAFE =====");
+
+            System.out.println("1. Tampilkan Inventori");
+            System.out.println("2. Tampilkan Stok untuk Item yang ada");
+            System.out.println("3. Tambah Item Baru");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih menu: ");
+            int pilihanMenu = sc.nextInt();
+
+            if (pilihanMenu == 1) {
+
+            } else if (pilihanMenu == 2) {
+
+            } else if (pilihanMenu == 3) {
+
+            } else if (pilihanMenu == 4) {
+                break OUTER_LOOP;
+            } else {
+                continue OUTER_LOOP;
+            }
+
+        }
     }
 }
